81b54f71c3cb693992908e9794d62f32308274fd
Nerto Kageh <kagehnerto@yahoo.com>
2022-12-15 09:45:42 -0600

    Initial Commit

---
+++ Elephant, Man, Ant Game/Elephant__Man__Ant_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elephant__Man__Ant_GameApp.swift
+//  Elephant, Man, Ant Game
+//
+//  Created by Induran Aderinto  on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Elephant__Man__Ant_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
